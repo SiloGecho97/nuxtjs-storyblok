90c44e867ef6e00781038cbf8229e71fedd3a761
Silas Getachew <silogecho@gmail.com>
2022-04-04 11:10:00 +0300

    Fix image alt

--- components/Payment.vue
+++ components/Payment.vue
@@ -2,7 +2,7 @@
   <div v-editable="blok" class="pb-8 mb-6 text-start p-4 bg-gray-100">
     <img
       class="h-10 w-10 mb-4 object-cover"
-      :src="blok.icons.filename" />
+      :src="blok.icons.filename" alt="Icons" />
     <h3 class="text-3xl font-bold my-2">{{ blok.name }}</h3>
     <p>{{ blok.description }}</p>
   </div>
